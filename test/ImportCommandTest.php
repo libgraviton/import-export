697317734a122748fcbcceb0140b52657f187ce5
Lucas Bickel <lucas.bickel@swisscom.com>
2015-08-25 00:52:04 +0200

    Split tests into error/non-error cases

--- test/ImportCommandTest.php
+++ test/ImportCommandTest.php
@@ -24,6 +24,71 @@ class ImportCommandTest extends \PHPUnit_Framework_TestCase
     /**
      * @dataProvider uploadFileProvider
      *
+     * @param string $host import target host with protocol
+     * @param string $file file to import
+     * @param string $path resulting path from file
+     *
+     * @return void
+     */
+    public function testUploadFile($host, $file, $path)
+    {
+        $clientMock = $this->getMockBuilder('GuzzleHttp\Client')->getMock();
+
+        $promiseMock = $this->getMock('GuzzleHttp\Promise\Promise');
+
+        $clientMock
+            ->method('requestAsync')
+            ->will($this->returnValue($promiseMock));
+
+        $responseMock = $this->getMock('Psr\Http\Message\ResponseInterface');
+
+        $responseMock
+            ->method('getHeader')
+            ->with('Link')
+            ->willReturn(['<' . $host . $path . '>; rel="self"']);
+
+        $promiseMock
+            ->method('then')
+            ->will(
+                $this->returnCallback(
+                    function ($ok) use ($responseMock) {
+                        $ok($responseMock);
+                    }
+                )
+            );
+
+        $sut = new ImportCommand(
+            new Finder(),
+            $clientMock,
+            new FrontMatter(),
+            new Parser(),
+            new VarCloner(),
+            new Dumper()
+        );
+
+        $cmdTester = $this->getTester($sut, $file);
+
+        $this->assertContains('Loading data from ' . $file, $cmdTester->getDisplay());
+        $this->assertContains('Wrote <' . $host . $path . '>; rel="self"', $cmdTester->getDisplay());
+    }
+
+    /**
+     * @return array[]
+     */
+    public function uploadFileProvider()
+    {
+        return [
+            'basic valid file' => [
+                'http://localhost',
+                __DIR__ . '/fixtures/set-01/test-2.json',
+                '/core/app/test',
+            ],
+        ];
+    }
+
+    /**
+     * @dataProvider errorFileProvider
+     *
      * @param string $host   import target host with protocol
      * @param string $file   file to import
      * @param string $path   resulting path from file
@@ -31,10 +96,8 @@ class ImportCommandTest extends \PHPUnit_Framework_TestCase
      *
      * @return void
      */
-    public function testUploadFile($host, $file, $path, $errors = [])
+    public function testErrorFile($host, $file, $path, $errors = [])
     {
-        $isError = !empty($errors);
-
         $clientMock = $this->getMockBuilder('GuzzleHttp\Client')->getMock();
 
         $promiseMock = $this->getMock('GuzzleHttp\Promise\Promise');
@@ -68,11 +131,8 @@ class ImportCommandTest extends \PHPUnit_Framework_TestCase
             ->method('then')
             ->will(
                 $this->returnCallback(
-                    function ($ok, $nok) use ($responseMock, $exceptionMock, $isError) {
-                        if ($isError) {
-                            return $nok($exceptionMock);
-                        }
-                        $ok($responseMock);
+                    function ($ok, $nok) use ($exceptionMock) {
+                        return $nok($exceptionMock);
                     }
                 )
             );
@@ -86,49 +146,23 @@ class ImportCommandTest extends \PHPUnit_Framework_TestCase
             new Dumper()
         );
 
-        $app = new Application();
-        $app->add($sut);
-
-        $cmd = $app->find('g:i');
-        $cmdTester = new CommandTester($cmd);
-        $cmdTester->execute(
-            [
-                'command' => $cmd->getName(),
-                'host' => 'http://localhost',
-                'file' => [
-                    $file
-                ],
-            ],
-            [
-                'decorated' => true
-            ]
-        );
+        $cmdTester = $this->getTester($sut, $file);
 
         $this->assertContains('Loading data from ' . $file, $cmdTester->getDisplay());
-
-        if ($isError) {
-            foreach ($errors as $error) {
-                $this->assertContains(
-                    $error,
-                    $cmdTester->getDisplay()
-                );
-            }
-        } else {
-            $this->assertContains('Wrote <' . $host . $path . '>; rel="self"', $cmdTester->getDisplay());
+        foreach ($errors as $error) {
+            $this->assertContains(
+                $error,
+                $cmdTester->getDisplay()
+            );
         }
     }
 
     /**
      * @return array[]
      */
-    public function uploadFileProvider()
+    public function errorFileProvider()
     {
         return [
-            'basic valid file' => [
-                'http://localhost',
-                __DIR__ . '/fixtures/set-01/test-2.json',
-                '/core/app/test',
-            ],
             'invalid file (server side)' => [
                 'http://localhost',
                 __DIR__ . '/fixtures/set-01/test.json',
@@ -147,5 +181,33 @@ class ImportCommandTest extends \PHPUnit_Framework_TestCase
                 ]
             ]
         ];
+    }
+
+    /**
+     * @param ImportCommand $sut  command under test
+     * @param string        $file file to load
+     *
+     * @return CommandTester
+     */
+    private function getTester(ImportCommand $sut, $file)
+    {
+        $app = new Application();
+        $app->add($sut);
+
+        $cmd = $app->find('g:i');
+        $cmdTester = new CommandTester($cmd);
+        $cmdTester->execute(
+            [
+                'command' => $cmd->getName(),
+                'host' => 'http://localhost',
+                'file' => [
+                    $file
+                ],
+            ],
+            [
+                'decorated' => true
+            ]
+        );
+        return $cmdTester;
     }
 }
